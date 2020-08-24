319403e869722fb2ec7de9fe9ceda4ca7bb2d8c2
marco <marco3jp+git@gmail.com>
2020-08-25 03:26:16 +0900

    一度きりの技以外でチェックを無効にした

--- components/BossController.vue
+++ components/BossController.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex justify-between boss-controller">
     <div @click="$emit('target-boss', 'BlackWing')" :class="{targeting: boss.name === 'BlackWing'}">羽</div>
-    <div @click="$emit('checked-action')">
-      <svg height="100%" viewBox="0 0 24 24">
+    <div @click="onClickCheckedAction">
+      <svg height="100%" viewBox="0 0 24 24" :class="{disabled: !isOnce}">
         <path fill="currentColor" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/>
       </svg>
     </div>
@@ -15,13 +15,25 @@
 
   export default Vue.extend({
     name: "BossController",
-    props: ['boss'],
+    props: ['boss', 'isOnce'],
+    methods: {
+      onClickCheckedAction() {
+        if (this.isOnce) {
+          this.$emit('checked-action');
+        }
+      }
+    }
   })
 </script>
 
 <style scoped lang="scss">
   .boss-controller {
     height: 100%;
+
+    .disabled {
+      color: rgba(189, 189, 189, 1);
+      transform: scale(0.75);
+    }
 
     & > div {
       text-align: center;
