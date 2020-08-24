3bf054b8768d250e25e02ba19394ea67436308f2
marco <marco3jp+git@gmail.com>
2020-08-25 03:24:59 +0900

    ターゲット中の敵名を太字・サイズ大きめに表示するよう変更

--- components/BossController.vue
+++ components/BossController.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex justify-between boss-controller">
-    <div @click="$emit('target-boss', 'BlackWing')">羽</div>
+    <div @click="$emit('target-boss', 'BlackWing')" :class="{targeting: boss.name === 'BlackWing'}">羽</div>
     <div @click="$emit('checked-action')">
       <svg height="100%" viewBox="0 0 24 24">
         <path fill="currentColor" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/>
       </svg>
     </div>
-    <div @click="$emit('target-boss', 'Lucilius')">ルシ</div>
+    <div @click="$emit('target-boss', 'Lucilius')" :class="{targeting: boss.name === 'Lucilius'}">ルシ</div>
   </div>
 </template>
 
@@ -14,7 +14,8 @@
   import Vue from 'vue';
 
   export default Vue.extend({
-    name: "BossController"
+    name: "BossController",
+    props: ['boss'],
   })
 </script>
 
@@ -25,6 +26,11 @@
     & > div {
       text-align: center;
       width: calc(100% / 3);
+    }
+
+    .targeting {
+      font-size: 1.2rem;
+      font-weight: bold;
     }
   }
 </style>
