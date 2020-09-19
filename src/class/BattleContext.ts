2a679b14c16efbf14b59c4f098a09f24dc1bf4cb
marco <marco3jp+git@gmail.com>
2020-09-19 20:49:41 +0900

    blackwingの行動もシミュレートできるようにした

--- src/class/BattleContext.ts
+++ src/class/BattleContext.ts
@@ -1,6 +1,7 @@
 import {battleContext, bossStatus} from "~/src/model/battleContext";
 import {BossStatus} from "~/src/class/BossStatus";
 import {LuciliusActions} from "~/src/LuciliusActions";
+import {BlackWingActions} from "~/src/BlackWingActions";
 
 export class BattleContext implements battleContext {
   boss: { Lucilius: bossStatus; BlackWing: bossStatus; countdown: number };
@@ -8,7 +9,7 @@ export class BattleContext implements battleContext {
   constructor() {
     this.boss = {
       Lucilius: new BossStatus("Lucilius", LuciliusActions),
-      BlackWing: new BossStatus("BlackWing", []),
+      BlackWing: new BossStatus("BlackWing", BlackWingActions),
       countdown: 6
     }
   }
